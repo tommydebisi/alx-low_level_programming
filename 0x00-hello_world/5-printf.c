32154247230b7be00301cae2e1b1affe537f6c8d
tommydebisi <tommydebisi@gmail.com>
2022-03-10 22:15:57 +0000

    task5 done

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints the string in the printf function
+ *
+ * Description: using the main function
+ * the program prints with proper grammar, but the outcome is a piece of art,
+ *
+ *Return: 0
+ */
+
+int main(void)
+{
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
