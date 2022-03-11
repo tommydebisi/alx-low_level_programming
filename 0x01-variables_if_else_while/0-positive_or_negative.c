479d1e691756c3c2fcee8ce98a9fa3dbe4d9355c
tommydebisi <tommydebisi@gmail.com>
2022-03-11 12:42:54 +0000

    task0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+/**
+ * main - prints the conditional if statements
+ * Description: the main function processes the if loop,
+ * if true it runs, if not it goes to else and prints the statement
+ *
+ * Return: 0
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	{
+		printf("%d is postive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	} /* your code goes there */
+	return (0);
+}
