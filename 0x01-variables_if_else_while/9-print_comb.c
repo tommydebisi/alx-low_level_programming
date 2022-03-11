e45cf03b52afb32863483a989866ad88d65f8fc0
tommydebisi <tommydebisi@gmail.com>
2022-03-11 19:44:46 +0000

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints the for loop
+ * Description: the main function processes the for loop,
+ * runs the for loop till it's false
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int dig;
+
+	for (dig = 0; dig <= 9; dig++)
+	{
+		putchar(dig', ');
+	}
+	putchar('\n');
+	return (0);
+}
