f8942a1d5425bf0efb0842d0ab38a9776ab4c760
tommydebisi <tommydebisi@gmail.com>
2022-03-11 19:24:12 +0000

    task 7 done

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints the for loop
+ * Description: the main function processes the for loop,
+ * runs the for loop till it's false
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int alp;
+
+	for (alp = 'z'; alp >= 'a'; alp--)
+	{
+		putchar(alp);
+	}
+	putchar('\n');
+	return (0);
+}
