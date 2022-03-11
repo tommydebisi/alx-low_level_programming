ed4a90cd2eaf98ef27d75b735a731cb360e91fcf
tommydebisi <tommydebisi@gmail.com>
2022-03-11 15:25:00 +0000

    task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints the for loop
+ * Description: the main function processes the for loop,
+ * runs the for loop till it's false
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char a[25];
+
+	for (a = 'a'; a <= 'z';	c++)
+	{
+		putchar(a);
+	}
+	return (0);
+}
