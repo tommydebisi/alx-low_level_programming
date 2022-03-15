5f371dcaabb9a9787391de661126eabb72c8fd7e
tommydebisi <tommydebisi@gmail.com>
2022-03-15 17:03:02 +0000

    task6 done

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *  _abs - _abs prints the absolute value
+ *  of an integer
+ * @int: gives the result of absolute value
+ *
+ * Return: @int for the result of the function
+ */
+int _abs(int)
+{
+	if (int < 0)
+	{
+		int = int * -1
+	}
+	return (int);
+}
