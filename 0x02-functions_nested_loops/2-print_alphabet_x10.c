607483b5c5da371a246e360ecd52da9d01e88ab4
tommydebisi <tommydebisi@gmail.com>
2022-03-15 15:24:08 +0000

    task2 done

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the character in putchar
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int dig = 0, alp;
+
+	while (dig < 10)
+	{
+		alp = 'a';
+		while (alp <= 'z')
+		{
+			_putchar(alp);
+			alp++
+		}
+		_putchar('\n');
+		dig++;
+	}
+	_putchar('\n');
+}
