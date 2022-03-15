82965e962b2e4daa968c25d00aeb0565f87e3d35
tommydebisi <tommydebisi@gmail.com>
2022-03-15 19:55:07 +0000

    task10 done

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - add funtion add the two integers together
+ *
+ * Return: @result
+ */
+
+int add(int i, int j)
+{
+	int result = i + j;
+	return (result);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * times_table -
+ *
+ */
+void times_table(void)
+{
+	int i = 0,j,k,l
+}
