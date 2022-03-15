6c3448234ce6a5e4dd9b1b6c32e9b5d18d153fb5
tommydebisi <tommydebisi@gmail.com>
2022-03-15 20:55:01 +0000

    task 11 done

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * print_to_98 - print_to_98 function prints
+ * natural numbers to ninety-eight
+ * @n: character we'll be inputting
+ */
+
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		while (n <= 98)
+		{
+			if (n == 98)
+			{
+				printf("%d\n", n);
+				break;
+			}
+			printf("%d, ", n);
+			n++;
+		}
+	}
+	else if (n >= 98)
+	{
+		while (n >= 98)
+		{
+			if (n == 98)
+			{
+				printf("%d\n", n);
+				break;
+			}
+			printf("d, ", n);
+			n--;
+		}
+	}
+}
