5cbb13a5538dcbdbab70e76d5f5d4513edb6d260
tommydebisi <tommydebisi@gmail.com>
2022-03-16 20:46:56 +0000

    task15 done

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - it prints the sum of even terms
+ * less than 4 million
+ * Return: 0
+ */
+int main(void)
+{
+	long int term1, term2, n, sum, result;
+
+	term1 = 1;
+
+	term2 = 2;
+
+	for (n = 0; n < 34; n++)
+	{
+		if (term1 % 2  == 0)
+		{
+			result += term1;
+		}
+		sum = term1 + term2;
+		term1 = term2;
+		term2 = sum;
+	}
+	printf("%lu\n", result);
+	return (0);
+}
