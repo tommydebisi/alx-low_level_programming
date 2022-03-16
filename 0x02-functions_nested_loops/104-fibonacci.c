0d64968ec31e5bd81daea93536611b9d3f5118f9
tommydebisi <tommydebisi@gmail.com>
2022-03-16 21:01:09 +0000

    task 16 done

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,33 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - it prints the first 50 numbers from
+ * one and two
+ * Return: 0
+ */
+int main(void)
+{
+	long int term1, term2, n, sum;
+
+	term1 = 1;
+
+	term2 = 2;
+
+	for (n = 0; n < 98; n++)
+	{
+		if (n != 97)
+		{
+			printf("%lu, ", term1);
+		}
+		else
+		{
+			printf("%lu\n", term1);
+		}
+		sum = term1 + term2;
+		term1 = term2;
+		term2 = sum;
+	}
+	return (0);
+}
