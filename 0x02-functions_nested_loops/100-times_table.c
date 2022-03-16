040d2dd9913e3734ec80b0449431aef7d1dcd278
tommydebisi <tommydebisi@gmail.com>
2022-03-16 01:35:07 +0000

    task10 done

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_times_table - print_times_table function prints tables
+ * @n: the multiplication table wanted
+ */
+void print_times_table(int n)
+{
+	int value, row, col, doub, unit;
+
+	row = 0;
+
+	if (n > 0 && n < 16)
+		while (row <= n)
+		{
+			col = 0;
+			while (col <= n)
+				value = row * col;
+				doub = value / 10;
+				unit = value % 10;
+				if (value < 10)
+					if (col != 0)
+						_putchar(' ');
+					_putchar(unit + '0');
+					if (col == n)
+						break;
+					_putchar(',');
+					_putchar(' ');
+				else
+					_putchar(doub + '0');
+					_putchar(unit + '0');
+					if (col == n)
+						break;
+					_putchar(',');
+					_putchar(' ');
+				col++;
+			_putchar('\n');
+			row++;
+		}
+}
