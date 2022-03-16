b308ddf3c0d2c1b8c202c00a97aef318b0eaa71e
tommydebisi <tommydebisi@gmail.com>
2022-03-16 00:44:42 +0000

    task 9 done

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,11 +1,47 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
- * times_table -
- *
+ * times_table - times_table function prints
+ * the times table from one to nine
  */
 void times_table(void)
 {
-	int i = 0,j,k,l
+	int value, row, col, doub, unit;
+
+	row = 0;
+
+	while (row < 10)
+	{
+		col = 0;
+		while (col < 10)
+		{
+			value = row * col;
+			doub = value / 10;
+			unit = value % 10;
+			if (value < 10)
+			{
+				putchar(unit + '0');
+				if (col == 9)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+			else
+			{
+				putchar(doub + '0');
+				putchar(unit + '0');
+				if (col == 9)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+			col++;
+		}
+		putchar('\n');
+		row++;
+	}
 }
