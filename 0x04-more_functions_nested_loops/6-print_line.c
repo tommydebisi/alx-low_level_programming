78af1175f1c2450f8ffe0fda056842b67d51527b
tommydebisi <tommydebisi@gmail.com>
2022-03-17 18:20:24 +0000

    task6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,20 +8,20 @@
 
 void print_line(int n)
 {
-for (n = 0; n < 11; n++)
-{
-if (n <= 0)
-{
-_putchar('\n');
-break;
-}
-else if (n > 0)
-{
-_putchar('_');
-if (n == 10)
-{
-_putchar('\n');
-}
-}
-}
+	for (n = 1; n < 11; n++)
+	{
+		if (n <= 0)
+		{
+			_putchar('\n');
+			break;
+		}
+		else if (n > 0)
+		{
+			_putchar('_');
+			if (n == 10)
+			{
+				_putchar('\n');
+			}
+		}
+	}
 }
