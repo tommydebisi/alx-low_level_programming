2befa9c1f5132f04440e4e3e0c4c8244bc4f13ba
tommydebisi <tommydebisi@gmail.com>
2022-03-17 11:18:31 +0000

    task 0 and readme added

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *_isupper - prints the return value if uppercase
+ *or lowercase
+ *@c: character to be tested
+ *Return: 1 if uppercase 0 if lowercase
+ */
+
+int _isupper(int c)
+{
+  if (c >= 'A' && c <= 'Z')
+    {
+      return (1);
+    }
+  else
+    {
+      return (0);
+    }
+}
