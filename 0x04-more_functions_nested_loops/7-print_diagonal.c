7afcb9e00ba36025e4cbb234749a26e9231954be
tommydebisi <tommydebisi@gmail.com>
2022-03-17 19:50:52 +0000

    task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*print_diagonal - prints \ diagonally to the console
+*@n: character to be entered
+*/
+
+void print_diagonal(int n)
+{
+int i;
+
+if (n > 0)
+{
+int j;
+
+j = n - 1;
+for (i = 0; i < j; i++)
+{
+_putchar(' ');
+}
+_putchar('\\');
+}
+_putchar('\n');
+}
