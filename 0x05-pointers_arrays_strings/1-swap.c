facddccf22d30e3bd6a1a65205c710b88b309d25
tommydebisi <tommydebisi@gmail.com>
2022-03-21 12:36:50 +0000

    task1 completed

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * swap_int - changes values
+ * @a: character to be inputed
+ * @b: second character to be inputted
+ */
+
+void swap_int(int *a, int *b)
+{
+*a = 42;
+*b = 98;
+}
