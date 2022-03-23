e5be614684fa0ebfdc0f9e772e6f4c7346c99091
tommydebisi <tommydebisi@gmail.com>
2022-03-23 20:43:27 +0000

    task1 added

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * _strncat - function adds two strings
+ * @dest: destination
+ * @src: source
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int len1, len2;
+/* i need to find the length of the first string*/
+len1 = 0;
+while (dest[len1] != '\0')
+{
+    len1++;
+}
+/*won't seal off here because we want to add more strings*/
+
+for (len2 = 0; len2 < n; len2++, len1++)
+{
+/*adds each character string of src printed to dest*/
+    if (src[len2] == ' ')
+    {
+        break;
+    }
+    
+    dest[len1] = src[len2];
+}
+/* to seal off the string */
+len1 = '\0';
+
+return (dest);
+}
