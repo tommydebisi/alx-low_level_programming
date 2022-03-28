1149b69b2ae9eccac254aa16099fd3ca4e4f4f52
tommydebisi <tommydebisi@gmail.com>
2022-03-28 14:50:03 +0000

    task2 added

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strchr - function returns the string from the first
+ * occurence of c
+ * @s: string of characters
+ * @c: character to be checked for
+ * Return: s
+ */
+
+char *_strchr(char *s, char c)
+{
+	/*Declare variables to be used in the for loops*/
+	int i;
+
+	/*loop through and get the first occurence*/
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		/*if gotten the first occurence return the address*/
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+	}
+	/*if after going through the loop there's no occurence of c return NULL*/
+	return ('\0');
+}
