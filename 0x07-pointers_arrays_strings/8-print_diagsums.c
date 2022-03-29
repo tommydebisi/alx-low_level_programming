4021f1e207fc1591f5133937e171c554fa5b393a
Adebisi Oluwatomiwa <57815158+tommydebisi@users.noreply.github.com>
2022-03-29 05:03:23 +0100

    task8 update2

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,31 +1,20 @@
 #include "main.h"
 #include <stdio.h>
-/**
- * print_diagsums - prints the values in the array
- * @a: integer to be inputted
- * @size: integer size of loop
- */
 
+/**
+ * print_diagsums -  prints sums of the two diagonals
+ * @a: a pointer
+ * @size: size
+ */
 void print_diagsums(int *a, int size)
 {
-	/*Declare variables for the loop*/
-	int row, col, i, res1, res2;
+	int i, res1 = 0, res2 = 0;
 
-	/*printing the row for first diagonal*/
-	for (row = 0; row < size; row++)
+	for (i = 0; i < size; i++)
 	{
-		/*printing the column*/
-		for (col = 0, i = 0; col < size; col++, i++)
-		{
-			/*check if row and column are equal*/
-			if (row == col)
-			{
-				res1 += a[row][col];
-			}
-			/*check if col is size - 1 - value of col, for the inverted diagonal*/
-			if (col == size - 1 - i
-		}
+		sum1 += *(a + (size * i + i));
+		sum2 += *(a + (size * i + size - 1 - i));
 	}
 	printf("%d, ", res1);
-	printf("%d\n", res1);
+	printf("%d\n", res2);
 }
