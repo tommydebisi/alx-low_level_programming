e11fa89ec44acea3eb5d3639001781a2a01f47f2
tommydebisi <tommydebisi@gmail.com>
2022-03-30 10:56:07 +0000

    readme, main.h and task0 added

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts_recursion - prints out all characters in a string
+ * @s: pointer to the string
+ */
+void _puts_recursion(char *s)
+{
+	/*using the tail recursion to imitate loop*/
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(++s);
+}
