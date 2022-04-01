c9d0c4fb96e7536ac8a99c14a64e1c6faffc433e
tommydebisi <tommydebisi@gmail.com>
2022-04-01 22:09:35 +0000

    task4 updated

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,22 +9,28 @@
  */
 int main(int argc, char *argv[])
 {
-	/*Declare variables for the loop and multiplication*/
-	int result = 0, i;
-
-	for (i = 0; i < argc; i++)
-	{
-		if ()
-		{
-			printf("Error\n");
-			return (1);
-		}
-		result += atoi(argv[i]);
-	}
-	if (i == 0)/*if the index is 0 after looping*/
+	/*Declare variables for the loop and addition*/
+	int result = 0, row, col;
+	
+	if (argc == 1)/*if the command argument is 1*/
 	{
 		printf("0\n");
 		return (0);
+	}
+	/*looping through the 2d array to get each elements*/
+	for (row = 1; row < argc; row++)
+	{
+		for (col = 0; argv[row][col] != '\0'; col++)
+		{
+			/*go through 1d array element and check if digit or not*/
+			if (!isdigit(argv[row][col]))/*isdigit checks characters if digit or not*/
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		/*when it leaves the loop it means all elements in the 1d array is digit*/
+		result += atoi(argv[row]);/*now we add the row without worrrying*/
 	}
 	printf("%d\n", result);/*prints addition of two arguments*/
 	return (0);
