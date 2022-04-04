312389ab53c94210275cde92f6e326cdfdc39790
tommydebisi <tommydebisi@gmail.com>
2022-04-04 12:08:33 +0000

    task1 update 2

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,20 +11,24 @@ char *_strdup(char *str)
 
 	char *ptr;
 
-	if (*str == '\0')
+	if (str == NULL)
 		return (NULL);
 	/*need to know the size of string*/
 	while (*(str + i) != '\0')
 		i++;
+	/*add 1 more so that \0 can be in the last space*/
+	i++;
 	/*typecast the return type to ptr*/
 	ptr = (char *)malloc(i * sizeof(char));
 	if (ptr == NULL)
 		return (NULL);
 	/*loop to duplicate the string*/
-	for (j = 0; j < i; j++)
+	for (j = 0; *(str + j) != '\0'; j++)
 	{
 		/*copying the values of str to ptr*/
 		*(ptr + j) = *(str + j);
 	}
+	*(ptr + j) = '\0';
+
 	return (ptr);
 }
