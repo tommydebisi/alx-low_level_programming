f3bdcd5777c2eab17e5ee5372bec2616b2f8f459
tommydebisi <tommydebisi@gmail.com>
2022-04-04 11:28:23 +0000

    task1 added

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _strdup - duplicates string specified
+ * @str: pointer to a string
+ * Return: ptr
+ */
+char *_strdup(char *str)
+{
+	unsigned int i = 0, j;
+
+	char *ptr;
+
+	if (str == NULL)
+		return (NULL);
+	/*need to know the size of string*/
+	while (*(str + i) != '\0')
+		i++;
+	/*typecast the return type to ptr*/
+	ptr = (char *)malloc(i * sizeof(char));
+	if (ptr == NULL)
+		return (NULL);
+	/*loop to duplicate the string*/
+	for (j = 0; j < i; j++)
+	{
+		/*copying the values of str to ptr*/
+		*(ptr + j) = *(str + j);
+	}
+	return (ptr);
+}
