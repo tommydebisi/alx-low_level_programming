6b2d43d70fa9ec6585ad4ab2b5cfa075d3ba76a5
tommydebisi <tommydebisi@gmail.com>
2022-04-07 16:28:30 +0000

    task3 added

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * array_range - prints the range of two integers with the integers included
+ * @min: integer to be passed
+ * @max: integer to be passed
+ * Return: ptr
+ */
+
+int *array_range(int min, int max)
+{
+	/*declare poiniter and variables to be used*/
+	int i, *ptr;
+	/*keying in requirements*/
+	if (min > max)
+		return (NULL);
+	/*creating memory spaces*/
+	ptr = malloc((max + 1) * sizeof(int));
+	/*added 1 to max so that max can have space to be in*/
+	if (ptr == NULL)
+		return (NULL);
+	/*now go through the loop and initialize the values*/
+	for (i = 0; i < max; i++)
+	{
+		/*increases the value of min till condition above it met*/
+		*(ptr + i) = min;
+		min++;
+	}
+	*(ptr + i) = max;/*max is added to the last memory spot*/
+	return (ptr);
+}
