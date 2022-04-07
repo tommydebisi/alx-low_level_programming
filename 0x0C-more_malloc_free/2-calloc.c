af0a122f4744c84705f02f066487214f266e4bc5
tommydebisi <tommydebisi@gmail.com>
2022-04-07 15:59:16 +0000

    task2 update1

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -22,7 +22,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (ptr == NULL)
 		return (NULL);
 	/*initialize all memory spaces with 0*/
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < (nmemb * size); i++)
 		*(ptr + i) = 0;
 	return (ptr);
 }
