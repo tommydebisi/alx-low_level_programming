4256475ead7c7f5ea5b9af2e6f91f6842db467e5
tommydebisi <tommydebisi@gmail.com>
2022-04-07 15:54:58 +0000

    task2 added

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _calloc - returns addres to memory spaces initialized with 0
+ * @nmemb: unsigned int
+ * @size: unsigned int
+ * Return: ptr
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	/*declare variables to be used*/
+	unsigned int i;
+
+	char *ptr;
+	/*setting requirements*/
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	/*get the memory spaces*/
+	ptr = (char *)malloc(nmemb * size);
+	if (ptr == NULL)
+		return (NULL);
+	/*initialize all memory spaces with 0*/
+	for (i = 0; i < nmemb; i++)
+		*(ptr + i) = 0;
+	return (ptr);
+}
