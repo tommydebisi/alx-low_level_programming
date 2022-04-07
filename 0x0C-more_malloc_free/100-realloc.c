146cf9d7fd7270ff153da52038bdba9bd5e88096
tommydebisi <tommydebisi@gmail.com>
2022-04-07 22:09:06 +0000

    task100 update1

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -14,21 +14,22 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == old_size)
 		return (ptr);
 	if (ptr == NULL)/*if null, just allocate space to the pointer*/
+	{
+		ptr = (char *)malloc(new_size);
+		if (ptr == NULL)
 		{
-			ptr = (char *)malloc(new_size);
-			if (ptr == NULL)
-			{
-				free(ptr);
-				return (NULL);
-			}
-			return (ptr);
+			free(ptr);
+			return (NULL);
 		}
+		return (ptr);
+	}
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	ptr = (char *)malloc(new_size);/*if all the statements are false*/
+	free(ptr);/*getting rid of the old memory initialized with ptr*/
+	ptr = (char *)malloc(new_size);/*if all the statements are false, create new*/
 	if (ptr == NULL)/*if it doesn't allocate space*/
 	{
 		free(ptr);
