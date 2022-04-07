681e69a7274961eb4cf4ea934875f21b629a3028
tommydebisi <tommydebisi@gmail.com>
2022-04-07 15:25:33 +0000

    task1 added

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,62 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * str_lent - gets the string length
+ * @s: pointer to the string
+ * Return: k
+ */
+int str_lent(char *s)
+{
+	/*declare variables to be used*/
+	int k = 0;
+
+	while (s[k] != '\0')
+		k++;
+	return (k);
+}
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: pointer to first string
+ * @s2: pointer to second string
+ * @n: unsigned int
+ * Return: ptr
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	/*declare pointer and variables to be used*/
+	unsigned int i, j, len1, len2, tot_len, comp;
+	char *ptr;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	/*we need length of the two strings*/
+	len1 = str_lent(s1);
+	len2 = str_lent(s2);
+	if (n >= len2)
+		len2 = n;
+	tot_len = len1 + n + 1;/*adding the length of 1st string and n given above*/
+	ptr = (char *)malloc(sizeof(char) * tot_len);
+	if (ptr == NULL)
+		return (NULL);
+	/*loop to add two strings to the memory*/
+	for (comp = i = j = 0; comp < tot_len; comp++)
+	{
+		/*check if first string is done copying*/
+		if (i < len1)
+		{
+			ptr[comp] = s1[i];
+			i++;
+		}
+		else
+		{
+			ptr[comp] = s2[j];/*starts copying 2nd string*/
+			j++;
+		}
+	}
+	ptr[comp] = '\0';
+	return (ptr);
+}
