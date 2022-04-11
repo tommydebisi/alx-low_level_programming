58b4af135a0677e75c0178353f59e26eb1eed8e0
tommydebisi <tommydebisi@gmail.com>
2022-04-11 22:00:57 +0000

    task4 update1

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,39 @@
 #include <stdlib.h>
 #include "dog.h"
+/**
+ * _strlen - prints length of string of array
+ * @s: character to be inputed
+ * Return: length
+ */
 
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	return (length);
+}
+/**
+ * *_strcpy - prints an array with size given
+ * @src: character to be inputed
+ * @dest: other character to be inputted
+ * Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int len;
+	/*go through loop and copy string to pointer*/
+	for (len = 0; *(src + len) != '\0'; len++)
+	{
+		dest[len] = *(src + len);
+	}
+	dest[len] = '\0';
+	return (dest);
+}
 /**
  * new_dog - stores new values in members
  * @name: pointer to string
@@ -11,6 +44,7 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	/*declare pointer to be used*/
+	int len1, len2;
 	dog_t *ptr;
 	/*allocating space for the pointer*/
 	ptr = (dog_t *)malloc(sizeof(dog_t));
@@ -19,9 +53,25 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(ptr);
 		return (NULL);
 	}
-	/*setting the strings and float to ptr*/
-	ptr->name = name;
+	/*get length of both strings*/
+	len1 = _strlen(name);
+	len2 = _strlen(owner);
+	/*malloc memory for both strings because they're pointer to strings*/
+	ptr->name = (char *)malloc(sizeof(char) * (len1 + 1));/*added 1 bcos of \0*/
+	if (ptr->name == NULL)
+	{
+		free(ptr->name);
+		return (NULL);
+	}
+	ptr->owner = (char *)malloc(sizeof(char) * (len2 + 1));/*added 1 bcos of \0*/
+	if (ptr->name == NULL)
+	{
+		free(ptr->owner);
+		return (NULL);
+	}
+	/*copy the strings to the malloc spaces created*/
+	_strcpy(ptr->name, name);
 	ptr->age = age;
-	ptr->owner = owner;
+	_strcpy(ptr->owner, owner);
 	return (ptr);
 }
