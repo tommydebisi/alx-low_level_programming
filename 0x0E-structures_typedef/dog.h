207dec1c31f8fab508ec8e58ef16ede7ddb1089c
tommydebisi <tommydebisi@gmail.com>
2022-04-11 13:31:30 +0000

    readme and task0 added

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - combines three members
+ * @name: first member
+ * @age: second member which displays age
+ * @owner: third member which displays owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /*DOG_H*/
