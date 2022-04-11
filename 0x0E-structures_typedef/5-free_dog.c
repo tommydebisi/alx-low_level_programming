02d4cde9ecdc7b42f9fa969a2655e7682225ad4f
tommydebisi <tommydebisi@gmail.com>
2022-04-11 22:15:35 +0000

    task5 added

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - frees all malloc spaces after use
+ * @d: struct pointer which used the malloc space
+ */
+void free_dog(dog_t *d)
+{
+	/*free the malloc created generally the for strings*/
+	free(d);
+	free(d->name);
+	free(d->owner);
+}
