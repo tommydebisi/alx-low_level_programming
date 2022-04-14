2796c79a956c586d02078d6521e01035fd13d070
tommydebisi <tommydebisi@gmail.com>
2022-04-14 01:29:21 +0000

    task4 update1

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -9,7 +9,8 @@
  */
 int main(int argc, char *argv[])
 {
-	int arg;
+	int arg, i;
+	char *arr;
 	/*key in requirements*/
 	if (argc != 2)
 	{
@@ -22,4 +23,15 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(2);
 	}
+	arr = (char *)main;
+	for (i = 0; i < bytes; i++)
+	{
+		if (i == bytes - 1)
+		{
+			printf("%02hhx\n", arr[i]);
+			break;
+		}
+		printf("%02hhx ", arr[i]);
+	}
+	return (0);
 }
