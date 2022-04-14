656ee8a415891922902c66a825dc6b0509e1c2e1
tommydebisi <tommydebisi@gmail.com>
2022-04-14 00:32:51 +0000

    task3 update5

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,5 +1,6 @@
 #include "3-calc.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * get_op_func - checks the element and initializes the op addresses
@@ -21,7 +22,7 @@ int (*get_op_func(char *s))(int, int)
 	/* go through struct array and check operator*/
 	while (i < 5)
 	{
-		if (*s == *ops[i].op)
+		if (strcmp(ops[i].op, s) == 0)
 			return (ops[i].f);
 		i++;
 	}
