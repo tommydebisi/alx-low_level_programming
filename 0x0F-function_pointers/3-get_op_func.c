3595ac4670771878a042fa6a398a76608493958d
tommydebisi <tommydebisi@gmail.com>
2022-04-14 00:16:22 +0000

    task3 update1

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -19,7 +19,7 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 	/* go through struct array and check operator*/
-	while (i < 5)
+	while (i < 6)
 	{
 		if (*s == *ops[i].op)
 			return (ops[i].f);
