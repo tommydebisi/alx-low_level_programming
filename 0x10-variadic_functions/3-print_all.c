c4a34f6c688a7f60fd3bbe7060b6dcd919b981ed
tommydebisi <tommydebisi@gmail.com>
2022-04-15 02:57:27 +0000

    task3 added

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,55 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
+#include "variadic_functions.h"
+
+/**
+ * print_all - print anthing
+ * @format: format which the program prints
+ */
+
+void print_all(const char * const format, ...)
+{
+	/*declare vairables to be used and va_list*/
+	int i = 0, j = 0, len;
+	char *s;
+	const char str[5] = {'c', 'i', 'f', 's', '\0'};
+	va_list arg;
+	/*getting the length of string specifier*/
+	len = strlen(format);
+	va_start(arg, format);
+	while (format[i] && format)/*format is not NULL and not \0*/
+	{
+		
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(arg, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(arg, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(arg, double));
+				break;
+			case 's':
+				s = va_arg(arg, char *);
+				if(!s)
+					printf("(nil)");
+				else
+					printf("%s", s);
+				break;
+		}
+		while (str[j])
+		{
+			if (format[i] == str[j] && i != (len - 1))
+				printf(", ");
+			j++;
+		}
+		j = 0;
+		i++;
+	}
+	printf("\n");
+	va_end(arg);
+
+}
