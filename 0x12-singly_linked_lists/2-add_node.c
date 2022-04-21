447752c765d1fd137858c01ba97da436570c8b59
tommydebisi <tommydebisi@gmail.com>
2022-04-21 15:11:56 +0000

    task2 added

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * add_node -  adds a new node at the beginning of a list
+ * @head: node pointer
+ * @str: pointer to string
+ * Return: address to new node
+ */
+
+
+list_t *add_node(list_t **head, const char *str)
+{
+	/*declare variables to be used*/
+	list_t *ptr;
+	unsigned int len = 0;
+	/*check if str is not empty*/
+	if (*str == '\0')
+		return (NULL);
+	/* getting length of string*/
+	while (str[len] != '\0')
+		len++;
+	/*malloc space for ptr*/
+	ptr = malloc(sizeof(list_t));
+	if (ptr == NULL)
+		return (NULL);
+	/*copying string to ptr*/
+	ptr->str = strdup(str);
+	ptr->len = len;
+	/*setting the address of struct pointer to head address*/
+	ptr->next = *head;
+	*head = ptr;
+	return (*head);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -4,17 +4,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+
 /**
  * struct node - struct with three members
  * @str: pointer to string
  * @len: integer
- * @next: pointer to struct
+ * @next: pointer to node
+ *
+ * Description: Singly list struct
  */
 
 typedef struct node
 {
 	char *str;
-	int len;
+	unsigned int len;
 	struct node *next;
 } list_t;
 
