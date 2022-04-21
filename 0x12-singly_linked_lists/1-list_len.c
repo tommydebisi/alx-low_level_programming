bf059becdc116f1ec84aa83aaa0945fd4e164639
tommydebisi <tommydebisi@gmail.com>
2022-04-21 13:31:57 +0000

    task1 added

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * list_len - return number of elements in a linked list
+ * @h: struct pointer
+ * Return: number of nodes
+ */
+size_t list_len(const list_t *h)
+{
+	/*declare variables to be used in loop and node*/
+	size_t count = 0;
+	/*go through loop and count*/
+	while (h != NULL)
+	{
+		h = h->next;/*h is the address of the next struct*/
+		count++;
+	}
+
+	return (count);
+}
