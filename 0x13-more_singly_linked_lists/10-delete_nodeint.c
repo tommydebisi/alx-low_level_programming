052519d60486504378d84d587193449edbad12e6
tommydebisi <tommydebisi@gmail.com>
2022-04-26 11:16:35 +0000

    task10 update2

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,11 +12,14 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	/*declare variables to be used*/
 	listint_t *current, *previous;
 	unsigned int i = 0;
+	/*check if head is NULL*/
+	if (*head == NULL)
+		return (-1);
 	/*set current to head*/
 	current = *head;
 	previous = NULL;
 	/*check if index is 0 which is the first node spot*/
-	if (index == 0 && *head != NULL)
+	if (index == 0)
 	{
 		/*move to next address*/
 		current = current->next;
@@ -26,14 +29,14 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (1);
 	}
 	/*traverse the list and get to the index position*/
-	while (i != index && *head != NULL)
+	while (i != index && current != NULL)
 	{
 		previous = current;
 		current = current->next;
 		i++;
 	}
-	/*check if current and previous are NULL*/
-	if (current == NULL && previous == NULL)
+	/*check if current is NULL*/
+	if (current == NULL)
 		return (-1);
 	/*set previous->next to be the next address after current*/
 	previous->next = current->next;
---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - reverse the lists
+ * @head: pointer to pointer to head
+ * Return: head
+ */
+
+listint_t *reverse_listint(listint_t **head)
+{
+	/*declare variables to be used*/
+	listint_t *prev, *current;
+	/*check if head is not NULL*/
+	if (*head == NULL)
+		return (NULL);
+}
