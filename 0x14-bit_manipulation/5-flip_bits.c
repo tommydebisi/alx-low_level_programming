04ee939233a1e907559ff9267439aebeea0705a3
tommydebisi <tommydebisi@gmail.com>
2022-04-28 23:40:32 +0000

    task5 added

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * flip_bit - prints number of times to flip bits to make int equal
+ * @n: number being changed to base two
+ * @m: second number being changed to base 2
+ * Return: count of flips
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int count = 0;
+	/*loop through till n and m are 0*/
+	while (n > 0 || m > 0)
+	{	
+		/*checking if index bit n and m are not equal to 0*/
+		if ((n & 1) != (m & 1))
+			count++;
+		/*move n and m to the next index bit*/
+		n >>= 1;
+		m >>= 1;
+	}
+	return (count);
+}
