a939040f395db3fb2f185b2f758f6f2ab2e03e6a
tommydebisi <tommydebisi@gmail.com>
2022-05-02 19:34:54 +0100

    task1 added

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * create_file - crreate a file
+ * @filename: file to be created
+ * @text_content: string to be added to file
+ * Return: 1 if successful -1 if not
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	/* Declare variables to be used */
+	int fd, i = 0;
+	/* set requirements */
+	if (!filename)
+		return (-1);
+	/* open the file */
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	if (text_content)
+	{
+		/* loop through and  */
+		while (text_content[i] != '\0')
+			i++;
+		write(fd, text_content, i);
+	}
+	close(fd);
+	return (1);
+}
