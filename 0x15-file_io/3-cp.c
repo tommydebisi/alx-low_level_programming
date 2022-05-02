40857c49b51006a9e838ffb9010b8eb65f51d27f
tommydebisi <tommydebisi@gmail.com>
2022-05-02 22:48:35 +0100

    task3 added

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,71 @@
+#include "main.h"
+/**
+ * error_check - checks for errors
+ * @fdfr: integer returned from read
+ * @fdto: integer returned from write
+ * @av: argument element
+ */
+void error_check(int fdfr, int fdto, char **av)
+{
+	/* check if the file was read */
+	if (fdfr == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+	/* check if file was written */
+	if (fdto == -1)
+	{
+		dprintf(2, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
+}
+/**
+ * main - Entry point
+ * @ac: arguments
+ * @av: arguments elements
+ * Return: Always 0.
+ */
+int main(int ac, char **av)
+{
+	/* Declare variables */
+	int numread = 1024, fdfr, fdto, clfr, clto;
+	char *buf;
+	/* check if arguments are correct */
+	if (ac != 3)
+	{
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	/* open and read the file */
+	fdfr = open(av[1], O_RDONLY);
+	/* open and wrtie to file_to */
+	fdto = open(av[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
+	/* mallocing space to buf */
+	buf = malloc(1024);
+	if (buf == NULL)
+		exit(0);
+	while (numread > 0)
+	{
+		/* read and give the number of char read */
+		numread = read(fdfr, buf, 1024);
+		/* write to the file */
+		write(fdto, buf, numread);
+	}
+	clfr = close(fdfr);
+	/* check if file descriptor for read closes */
+	if (clfr == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d", fdfr);
+		exit(100);
+	}
+	/* check if file descriptor for write closes */
+	clto = close(fdto);
+	if (clto == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d", fdto);
+		exit(100);
+	}
+	free(buf);
+	return (0);
+}
