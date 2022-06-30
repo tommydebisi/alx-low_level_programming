6cc1762a55cfbe1cbaf60e73170ec0ec68042bc5
tommydebisi <tommydebisi@gmail.com>
2022-07-01 00:44:40 +0100

    :construction: task2 added

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - gets the index of a key
+ *
+ * @key: the key to identified with
+ * @size: sie of array
+ * Return: key index
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
