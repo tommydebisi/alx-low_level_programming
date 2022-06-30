650b6a493d4a988a3e2c32c9eda2d33370b60969
tommydebisi <tommydebisi@gmail.com>
2022-06-30 18:30:07 +0100

    :seedling: init htable size

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -4,7 +4,6 @@
  * hash_table_create - creates a hastable
  *
  * @size: Size of the hash table
- *
  * Return: pointer to the newly create hash table
  * or NULL if something happens
  */
@@ -13,13 +12,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *htable;
 	unsigned long int i;
 
-	/* init size of hashtable */
-	htable->size = size;
-
 	/* Malloc space for one hashtable */
 	htable = malloc(sizeof(hash_table_t) * 1);
 	if (!htable)
 		return (NULL);
+
+	/* init size of hashtable */
+	htable->size = size;
 
 	/* Malloc space for the array in the table */
 	htable->array = malloc(htable->size * sizeof(hash_table_t *));
